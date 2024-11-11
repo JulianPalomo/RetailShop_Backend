aa4ee111e71c0827c689093be0afecdf812a54f8
Julian Palomo <88567929+JulianPalomo@users.noreply.github.com>
2024-11-11 19:15:10 -0300

    Update SaleDto.java

--- src/main/java/com/finalProyect/retailShop_Backend/DTO/SaleDto.java
+++ src/main/java/com/finalProyect/retailShop_Backend/DTO/SaleDto.java
@@ -13,12 +13,32 @@ import java.util.List;
 
 
 //TODO : AGREGAR CLIENTID(NUMERO DE DNI);
+/*TODO: guiarse por estos tipos de dato:   export interface Sale{
+    id: number;
+    seller: string;
+    client: number;
+    products: cartProduct[];
+    total: number;
+    date: Date;
+    paymentMethod: string;
+  }
+  export interface cartProduct {
+    sku: number;
+    cartId: number;
+    description: string;
+    quantity: number;
+    unitPrice: number;
+    subTotal: number;
+  }
+  
+*/
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SaleDto {
     private Long id;
     private Long userId;  // ID del usuario que realiza la venta
+    //En vez de guardar un cartProductEntity, guardar un CartProductDto con la data de mi CartProduct.
     private List<CartProductEntity> products;  // Lista de productos en la venta
     private BigDecimal total;  // Total de la venta
     private LocalDate date;  // Fecha de la venta
