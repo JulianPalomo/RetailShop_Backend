e3bfa646b26a0d0916e8214f7421ceb69e94ba2f
alvaroatue <163065658+alvaroatue@users.noreply.github.com>
2024-11-13 18:41:51 -0300

    a
    a

--- src/main/java/com/finalProyect/retailShop_Backend/mappers/SaleMapper.java
+++ src/main/java/com/finalProyect/retailShop_Backend/mappers/SaleMapper.java
@@ -1,45 +1,50 @@
 package com.finalProyect.retailShop_Backend.mappers;
 
 import com.finalProyect.retailShop_Backend.DTO.CartProductDto;
-import com.finalProyect.retailShop_Backend.DTO.ProductDto;
 import com.finalProyect.retailShop_Backend.DTO.SaleDto;
 import com.finalProyect.retailShop_Backend.entities.SaleEntity;
-import com.finalProyect.retailShop_Backend.entities.products.ProductEntity;
-import com.finalProyect.retailShop_Backend.services.UserService;
-import lombok.RequiredArgsConstructor;
+import com.finalProyect.retailShop_Backend.entities.persons.UserEntity;
+import com.finalProyect.retailShop_Backend.entities.products.CartProductEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.stream.Collectors;
-
 @Component
-@RequiredArgsConstructor
 public class SaleMapper {
 
-    private final UserService userService;
+    private final CartProductMapper cartProductMapper;
 
-    public SaleEntity toEntity(SaleDto saleDto) {
-        return SaleEntity.builder()
-                //.user(userService.getUserById(saleDto.getUserId())) // Obtiene el usuario usando su ID
-                .products(saleDto.getProducts().stream() // Convierte la lista de ProductDto a ProductEntity
-                        .map(this::convertToProductEntity) // Llama al método que creamos
-                        .collect(Collectors.toList())) // Recoge los elementos convertidos en una lista
-                .total(saleDto.getTotal())
-                .date(saleDto.getDate())
-                .paymentMethod(saleDto.getPaymentMethod())
-                .build();
+    public SaleMapper(CartProductMapper cartProductMapper) {
+        this.cartProductMapper = cartProductMapper;
     }
 
-    private ProductEntity convertToProductEntity(ProductDto productDto) {
-        return ProductEntity.builder()
-                .id(productDto.getId())
-                .name(productDto.getName())
-                .price(productDto.getPrice())
-                .stock(productDto.getStockQuantity())
-                .build();
+    public SaleDto toDto(SaleEntity sale) {
+        SaleDto dto = new SaleDto();
+        dto.setId(sale.getId());
+        dto.setUserId(sale.getUser().getId());
+        dto.setProducts(sale.getProducts().stream()
+                .map(cartProductMapper::toDto)
+                .collect(Collectors.toList()));
+        dto.setTotal(sale.getTotal());
+        dto.setDate(sale.getDate());
+        dto.setPaymentMethod(sale.getPaymentMethod());
+        return dto;
     }
 
-    public SaleDto toDto(SaleEntity saleEntity) {
-        return new SaleDto(
-        );
+    public SaleEntity toEntity(SaleDto dto, UserEntity user, List<CartProductEntity> cartProducts) {
+        SaleEntity sale = new SaleEntity();
+        sale.setId(dto.getId());
+        sale.setUser(user);
+        sale.setProducts(cartProducts);
+        sale.setTotal(dto.getTotal());
+        sale.setDate(dto.getDate());
+        sale.setPaymentMethod(dto.getPaymentMethod());
+        return sale;
+    }
+
+    public List<CartProductDto> toDtoList(List<CartProductEntity> entityList) {
+        return entityList.stream()
+                .map(cartProductMapper::toDto)
+                .collect(Collectors.toList());
     }
 }
