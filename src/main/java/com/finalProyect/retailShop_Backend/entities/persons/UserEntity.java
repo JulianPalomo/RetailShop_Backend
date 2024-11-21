179b73fd04a7aa4abfb3cc7f74b4ac9a1cd6b01a
Lucrecia Martin <lucre.martin01@gmail.com>
2024-11-21 02:22:40 -0300

    error en entidad sale y la relacion con cart sale

---
+++ src/main/java/com/finalProyect/retailShop_Backend/entities/persons/UserEntity.java
@@ -0,0 +1,25 @@
+package com.finalProyect.retailShop_Backend.entities.persons;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "users")
+@Data  // Genera autoincrement getters, setters, equals, hashcode y toString
+@NoArgsConstructor  // Genera un constructor sin argumentos
+@AllArgsConstructor  // Genera un constructor con todos los argumentos
+@SuperBuilder  // Facilita la creación de objetos con el patrón builder
+public class UserEntity extends PersonEntity {
+
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true,nullable = false)
+    private String dni;
+
+    @Column(nullable = false)
+    private boolean isAdmin;
+
+}
